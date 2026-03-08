e9d4e10a7f048ff04adbce72e0e859a1b08338fd
csmohamme <cs.mohammedalmostafa@gmail.com>
2026-03-08 12:36:13 +0300

    Get all categories

---
+++ CodingWiki_Web/Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categories = _db.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
