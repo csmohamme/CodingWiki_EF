19cbb6369daf97b1be2746a39125f6c544f7ed85
csmohamme <cs.mohammedalmostafa@gmail.com>
2026-03-09 15:14:34 +0300

    Author CRUD operation

---
+++ CodingWiki_Web/Controllers/AuthorController.cs
@@ -0,0 +1,69 @@
+﻿using CodingWiki_DataAccess.Data;
+using CodingWiki_Model.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingWiki_Web.Controllers
+{
+    public class AuthorController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public AuthorController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Author> authors = _db.Authors.ToList();
+            return View(authors);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            Author author = new Author();
+            if (id == null)
+            {
+                return View(author);
+            }
+            author = _db.Authors.Find(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            return View(author);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Upsert(Author author)
+        {
+            if (ModelState.IsValid)
+            {
+                if (author.Author_Id == 0)
+                {
+                    await _db.Authors.AddAsync(author);
+                }
+                else
+                {
+                    _db.Authors.Update(author);
+                }
+
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            Author author = _db.Authors.Find(id);
+            if (author == null)
+            {
+                return NotFound();
+            }
+            _db.Authors.Remove(author);
+            await _db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+    }
+}
